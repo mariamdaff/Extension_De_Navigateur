1a1dacce94a019114188951b657d662bb03c9fd2
Maya <maeva.berthail@student.adatechschool.fr>
2025-01-08 11:29:25 +0100

    Add onInstalled listener in notifs.js to test the notifs, and link to notifs.js in manifest. Basic notif tested and working

--- Notifications/notifs.js
+++ Notifications/notifs.js
@@ -1,10 +1,13 @@
-chrome.notifications.create('drinking-reminder', {
-	type: 'basic',
-	iconUrl: 'Ressources/icons/notif.png',
-	title: 'Time to drink water !',
-	message: "It's time to drink your next glass !\nWhat do you want to do ?",
-	buttons: [
-		{ title: 'Remind me in 10 !', iconUrl: 'Ressources/icons/time-forward-10.png' },
-		{ title: 'Drinking now !', iconUrl: 'Ressources/icons/water-glass.png' }
-	  ]
-  });
+chrome.runtime.onInstalled.addListener(() => {
+	console.log('Extension installed');
+	chrome.notifications.create('drinking-reminder', {
+		type: 'basic',
+		iconUrl: 'Ressources/icons/notif.png',
+		title: 'Time to drink water !',
+		message: "It's time to drink your next glass !\nWhat do you want to do ?",
+		buttons: [
+			{ title: 'Remind me in 10 !', iconUrl: 'Ressources/icons/time-forward-10.png' },
+			{ title: 'Drinking now !', iconUrl: 'Ressources/icons/water-glass.png' }
+	 	]
+  	});
+});  
